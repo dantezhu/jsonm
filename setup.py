14bb3b63ba3e328bfbf7f616b8c7dd9b3ed82398
dantezhu <dantezhu@qq.com>
2015-01-24 11:38:25 +0800

    升级版本号

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="jsonm",
-    version="1.0.5",
+    version="1.0.6",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=[],
     scripts=[],
