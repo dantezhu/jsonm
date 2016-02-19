3d2b0dc6f85ecbce457551a08eb805dc6b0f74af
dantezhu <dantezhu@qq.com>
2016-02-19 17:08:27 +0800

    支持多个jsonm实例

---
+++ examples/jsonm_app_demo/desk.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from jsonm import Field, Model
+from vals import jsonm_app
+
+
+class Desk(Model):
+
+    __jsonm_app__ = jsonm_app
+
+    id = Field()
+    current_uin = Field(default=-1)
+
+    def __init__(self, id=None):
+        super(Desk, self).__init__()
+        # 默认
+        self.id = id
+
+    def on_from_json_over(self):
+        print '%s.on_from_json_over' % self.__class__
+
+    def on_to_json_over(self, json_value):
+        print '%s.on_to_json_over: %s' % (self.__class__, json_value)
---
+++ examples/jsonm_app_demo/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.insert(0, '../../')
+import datetime
+from desk import Desk
+from vals import jsonm_app
+
+
+def main():
+    jsonm_app.register_models((Desk,))
+
+    desk = Desk(7)
+    desk.current_uin = 1
+
+    str_json = jsonm_app.json_dumps(desk, indent=4)
+
+    print str_json
+
+    desk = jsonm_app.json_loads(str_json)
+
+    print desk
+
+if __name__ == '__main__':
+    main()
