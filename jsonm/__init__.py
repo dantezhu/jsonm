5381919172c414bf0a4bf96796f33a65867e18ef
朱念洋 <dantezhu@dantezhuMBP.lan>
2016-12-14 19:14:08 +0800

    版本升级

--- jsonm/__init__.py
+++ jsonm/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.25'
+__version__ = '1.0.27'
 
 from .application import Application
 from .fields import BaseField, Field
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="jsonm",
-    version="1.0.25",
+    version="1.0.27",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=[],
     scripts=[],
